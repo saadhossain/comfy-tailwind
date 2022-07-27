f6727c3121a077e3cd0e1cdf0cfd45a5ddd70f4a
saadhossain <mshsarkar@gmail.com>
2022-07-27 17:38:18 +0600

    Header navigation and header banner added

---
+++ comfy.js
@@ -0,0 +1,15 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+            comfybg: '#FFF9F4',
+            comfytext:'#FA804C',
+            comfygray: '#F1F1F1',
+        }
+      }
+    }
+  }
+
+//   for button bg color
+
+//   bg-gradient-to-r from-sky-500 to-indigo-500
